d1826cddc14996b34e6e26071b863f79174f0271
reginopm <reginopm0897@gmail.com>
2021-07-16 12:20:26 -0500

    Agregado de notas en notes.txt y código en playlist.js

---
+++ playlist.js
@@ -0,0 +1,5 @@
+var list = "Música romántica";
+var album = "Salsa Colombiana";
+var cancion = "El negrito de la salsa";
+var author = "Édgar Gonzalón"
+console.log("Lista: ", list, "\nAlbum: ", album, "\nCanción: ", cancion, "\nAutor: ", author);
